83abbb7cd5f6b3cfd5a365a4e10c0b59878a3611
tikhon <tikhon.radk@gmail.com>
2024-10-04 17:13:50 +0300

    stat model (daily mean) created

---
+++ storesales/baseline/stat_models.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+class DailyMeanModel:
+    def __init__(self, window: int):
+        self.window = window
+        self.prediction = None
+
+    def fit(self, train: pd.DataFrame) -> None:
+        self.prediction = train["y"].tail(self.window).mean()
+
+    def predict(self, future: pd.DataFrame) -> pd.DataFrame:
+        if self.prediction is None:
+            raise ValueError("Model not fitted")
+        future["yhat"] = [self.prediction] * len(future)
+        return future
