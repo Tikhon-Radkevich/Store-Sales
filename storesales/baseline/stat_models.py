8b2d415fc8789fc2ddee6794bd43a21e79b928b5
tikhon <tikhon.radk@gmail.com>
2024-10-06 14:19:01 +0300

    DayOfWeekMeanModel added

--- storesales/baseline/stat_models.py
+++ storesales/baseline/stat_models.py
@@ -15,3 +15,28 @@ class DailyMeanModel:
         prediction = future.copy()
         prediction["yhat"] = [self.prediction] * len(future)
         return prediction
+
+
+class DayOfWeekMeanModel:
+    def __init__(self, weekdays_window: int, weekends_window: int):
+        self.weekdays_window = weekdays_window
+        self.weekends_window = weekends_window
+        self.prediction = None
+
+    def fit(self, train: pd.DataFrame) -> None:
+        weekdays = train[train["ds"].dt.weekday < 5]
+        weekends = train[train["ds"].dt.weekday >= 5]
+        self.prediction = {
+            "weekdays": weekdays["y"].tail(self.weekdays_window).mean(),
+            "weekends": weekends["y"].tail(self.weekends_window).mean(),
+        }
+
+    def predict(self, future: pd.DataFrame) -> pd.DataFrame:
+        if self.prediction is None:
+            raise ValueError("Model not fitted")
+        prediction = future.copy()
+        prediction["yhat"] = [
+            self.prediction["weekdays"] if day < 5 else self.prediction["weekends"]
+            for day in future["ds"].dt.weekday
+        ]
+        return prediction
