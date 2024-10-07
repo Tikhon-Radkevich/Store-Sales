90a9b9b096f6006d7d8bcce9eefd08ebef79ecc8
tikhon <tikhon.radk@gmail.com>
2024-10-07 11:41:06 +0300

    baseline family groups list created; n_stores constant added

---
+++ storesales/baseline/constants.py
@@ -0,0 +1,29 @@
+FAMILY_GROUPS = [
+    ("AUTOMOTIVE", "CLEANING", "DELI", "GROCERY I", "PERSONAL CARE", "PREPARED FOODS"),
+    ("BEAUTY", "BEVERAGES", "BREAD/BAKERY", "DAIRY", "EGGS", "POULTRY"),
+    (
+        "CELEBRATION",
+        "HOME CARE",
+        "LADIESWEAR",
+        "PET SUPPLIES",
+        "PLAYERS AND ELECTRONICS",
+        "PRODUCE",
+    ),
+    ("BABY CARE",),
+    ("BOOKS",),
+    ("FROZEN FOODS",),
+    ("GROCERY II",),
+    ("HARDWARE",),
+    ("HOME AND KITCHEN I",),
+    ("HOME AND KITCHEN II",),
+    ("HOME APPLIANCES",),
+    ("LAWN AND GARDEN",),
+    ("LINGERIE",),
+    ("LIQUOR,WINE,BEER",),
+    ("MAGAZINES",),
+    ("MEATS",),
+    ("SCHOOL AND OFFICE SUPPLIES",),
+    ("SEAFOOD",),
+]
+
+N_STORES = 54
