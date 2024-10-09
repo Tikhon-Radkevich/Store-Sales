2b2b253e6ad2b49edeb277915145c8c4fcd4c0ef
tikhon <tikhon.radk@gmail.com>
2024-10-09 11:24:00 +0300

    refactoring; small fix (family group type: set[str] -> tuple[str])

--- storesales/baseline/sales_predictor.py
+++ storesales/baseline/sales_predictor.py
@@ -16,60 +16,61 @@ class SalesPredictor:
         self,
         model_wrappers: dict[str, ModelBaseWrapper],
         inner_cutoffs: list[int],
-        family_groups: list[set[str]],
+        family_groups: list[tuple[str]],
         outer_cv: TimeSeriesSplit,
         optuna_optimize_kwargs: dict,
         n_group_store_family_choices: int = 4,
         n_single_store_family_choices: int = 2,
+        horizon: str = "16 days",
     ):
         self.model_wrappers = model_wrappers
         self.inner_cutoffs = inner_cutoffs
         self.family_groups = family_groups
-
+        self.outer_cv = outer_cv
         self.optuna_optimize_kwargs = optuna_optimize_kwargs
         self.n_group_store_family_choices = n_group_store_family_choices
         self.n_single_store_family_choices = n_single_store_family_choices
-        self.outer_cv = outer_cv
+        self.horizon = horizon
 
-        self.tune_storage = self._initiate_tune_storage()
-        self.best_storage = {}
+        self.tune_storage = self._initialize_tune_storage()
+        self.family_to_madel_params_storage = {}
+        self.store_family_to_model_storage = {}
 
-        self.models = {}
+    def _initialize_tune_storage(self) -> dict:
+        return {family_group: defaultdict(list) for family_group in self.family_groups}
 
-    def _initiate_tune_storage(self):
-        tune_storage = {}
-        for family_group in self.family_groups:
-            tune_storage[family_group] = defaultdict(list)
-        return tune_storage
-
-    def get_n_store_family_choices(self, family_group):
+    def get_n_store_family_choices(self, family_group: tuple[str]) -> int:
         if len(family_group) == 1:
             return self.n_single_store_family_choices
         return self.n_group_store_family_choices
 
-    def fit(self, train: pd.DataFrame, initial: str) -> None:
-        if not self.best_storage:
+    def fit(self, train: pd.DataFrame, initial: str = "760 days") -> None:
+        if not self.family_to_madel_params_storage:
             raise ValueError("Sales Predictor has not been tuned.")
 
         store_nbrs = train["store_nbr"].unique()
-        # if not self.models:
-        for family, args in self.best_storage.items():
-            for store_nbr in store_nbrs:
-                model = self.get_best_model(args["params"])
-                self.models[(store_nbr, family)] = model
 
-        last_730_days = train[train["ds"] >= train["ds"].max() - pd.Timedelta(initial)]
-        for (store_nbr, family), model in tqdm(self.models.items()):
-            x_train = last_730_days[
-                (last_730_days["store_nbr"] == store_nbr)
-                & (last_730_days["family"] == family)
+        for family, model_params in self.family_to_madel_params_storage.items():
+            for store_nbr in store_nbrs:
+                model = self.get_best_model(model_params["params"])
+                self.store_family_to_model_storage[(store_nbr, family)] = model
+
+        train_slice = train[train["ds"] >= train["ds"].max() - pd.Timedelta(initial)]
+        for (store_nbr, family), model in tqdm(
+            self.store_family_to_model_storage.items()
+        ):
+            x_train = train_slice[
+                (train_slice["store_nbr"] == store_nbr)
+                & (train_slice["family"] == family)
             ]
             model.fit(x_train)
 
     def predict(self, test: pd.DataFrame, submission: pd.DataFrame) -> pd.DataFrame:
         prediction_list = []
 
-        for (store_nbr, family), model in tqdm(self.models.items()):
+        for (store_nbr, family), model in tqdm(
+            self.store_family_to_model_storage.items()
+        ):
             x_test = test[(test["store_nbr"] == store_nbr) & (test["family"] == family)]
 
             forecast = model.predict(x_test)[["ds", "yhat"]]
@@ -91,7 +92,7 @@ class SalesPredictor:
 
     def evaluate_and_save_tune(
         self,
-        family_group: set[str],
+        family_group: tuple[str],
         best_params: dict,
         train: pd.DataFrame,
         test: pd.DataFrame,
@@ -101,33 +102,38 @@ class SalesPredictor:
         forecast = model.predict(test)
         y_pred = forecast["yhat"].values
         y_true = test["y"].values
-        loss = self.rmsle(y_true, y_pred)
+        loss = rmsle(y_true, y_pred)
 
         self.tune_storage[family_group]["loss"].append(loss)
         for key, value in best_params.items():
             self.tune_storage[family_group][key].append(value)
         return loss
 
-    def log_best(self, family_group: set[str]):
-        tune_run = self.tune_storage[family_group]
-        loss = np.mean(tune_run["loss"])
+    def log_best(self, family_group: tuple[str]):
+        best_params = self.tune_storage[family_group]
+        loss = np.mean(best_params["loss"])
 
-        values, counts = np.unique(tune_run["model"], return_counts=True)
+        values, counts = np.unique(best_params["model"], return_counts=True)
         best_model = str(values[np.argmax(counts)])
 
-        best_params = self.model_wrappers[best_model].get_mean_params(
-            tune_run, best_model
+        best_mean_params = self.model_wrappers[best_model].get_mean_params(
+            best_params, best_model
         )
 
         for family in family_group:
-            self.best_storage[family] = {"params": best_params, "loss": loss}
+            self.family_to_madel_params_storage[family] = {
+                "params": best_mean_params,
+                "loss": loss,
+            }
 
     def objective(self, trial: optuna.Trial, train: pd.DataFrame) -> float:
         model_name = trial.suggest_categorical(
             "model", list(self.model_wrappers.keys())
         )
         cutoffs = train["ds"].iloc[self.inner_cutoffs].reset_index(drop=True)
-        return self.model_wrappers[model_name].objective(trial, train, cutoffs)
+        return self.model_wrappers[model_name].objective(
+            trial, train, cutoffs, self.horizon
+        )
 
     def get_best_model(self, best_params):
         best_params = best_params.copy()
@@ -136,13 +142,3 @@ class SalesPredictor:
 
     def render_model(self, params) -> str:
         return self.model_wrappers[params["model"]].render(**params)
-
-    def get_best(self, best_model_name: str, run_storage) -> dict:
-        best_params = run_storage[best_model_name]
-        mean_params = self.model_wrappers[best_model_name].get_mean_params(
-            best_params, best_model_name
-        )
-        return mean_params
-
-    def rmsle(self, y_true, y_pred):
-        return rmsle(y_true, y_pred)
