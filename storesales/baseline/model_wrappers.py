eb238d062eb12760263fb0d57f2cc380cd5587b5
tikhon <tikhon.radk@gmail.com>
2024-10-09 11:01:34 +0300

    ModelBaseWrapper class:   * refactoring:     - __init__() update: default values ([]) for suggestions attrs;     - suggest_params() and get_mean_params() optimization (using default empty lists);     - objective(): harcoded test slice is replaced with Timedelta(horizon).
    DailyMeanModelWrapper && DayOfWeekMeanModelWrapper classes:
      * refactoring (small changes in __init__())
    
    ProphetWrapper class:
      * refactoring:
        - __init__(): facebook prophet default params are moved out of class (now `model_base_params` in __init__ args);
        - _process_model_before_fit() update (using default `extra_regressors` empty list value);
    
      * fix:
        - objective(): now fit model before `cross_validation` with all train period; `horizon` model out of class.

--- storesales/baseline/model_wrappers.py
+++ storesales/baseline/model_wrappers.py
@@ -23,86 +23,75 @@ class ModelBaseWrapper:
         categorical_suggestions: list[CategoricalSuggestions] = None,
         model_base_params: dict = None,
     ):
-        self.int_suggestions = int_suggestions
-        self.float_suggestions = float_suggestions
-        self.categorical_suggestions = categorical_suggestions
-        self.model_base_params = (
-            model_base_params if model_base_params is not None else {}
-        )
+        self.int_suggestions = int_suggestions or []
+        self.float_suggestions = float_suggestions or []
+        self.categorical_suggestions = categorical_suggestions or []
+        self.model_base_params = model_base_params or {}
         self.estimator = estimator
 
     def get_model(self, **kwargs):
-        model_params = self.model_base_params.copy()
-        model_params.update(kwargs)
-
-        model = self.estimator(**model_params)
-
-        model = self._process_model_before_fit(model)
-
-        return model
+        params = {**self.model_base_params, **kwargs}
+        model = self.estimator(**params)
+        return self._process_model_before_fit(model)
 
     def objective(
-        self, trial: optuna.Trial, df: pd.DataFrame, cutoffs: pd.DataFrame
+        self, trial: optuna.Trial, df: pd.DataFrame, cutoffs: pd.DataFrame, horizon: str
     ) -> float:
         model = self.get_model(**self.suggest_params(trial))
-
         losses = []
+
         for cutoff in cutoffs:
-            train_condition = df["ds"] < cutoff
-            train = df[train_condition]
-            future = df[~train_condition][:16]
+            train_data = df[df["ds"] < cutoff]
+            future_data = df[
+                (df["ds"] >= cutoff) & (df["ds"] < cutoff + pd.Timedelta(horizon))
+            ]
 
-            model.fit(train)
-            forecast = model.predict(future)
+            model.fit(train_data)
+            forecast = model.predict(future_data)
 
+            y_true = future_data["y"].values
             y_pred = forecast["yhat"].values
-            y_true = forecast["y"].values
-
             losses.append(rmsle(y_true, y_pred))
 
         return np.mean(losses)
 
-    def _process_model_before_fit(self, model):
-        return model
-
     def suggest_params(self, trial: optuna.Trial) -> dict:
-        params = {}
-        if self.int_suggestions:
-            for suggestion in self.int_suggestions:
-                params[suggestion.name] = trial.suggest_int(
-                    suggestion.name, suggestion.low, suggestion.high
-                )
-        if self.float_suggestions:
-            for suggestion in self.float_suggestions:
-                params[suggestion.name] = trial.suggest_float(
-                    suggestion.name, suggestion.low, suggestion.high
-                )
-        if self.categorical_suggestions:
-            for suggestion in self.categorical_suggestions:
-                params[suggestion.name] = trial.suggest_categorical(
-                    suggestion.name, suggestion.choices
-                )
-        return params
+        suggested_params = {}
 
-    def get_mean_params(self, best_params: dict, best_model_name: str) -> dict:
-        mean_params = {"model": best_model_name}
+        for suggestion in self.int_suggestions:
+            suggested_params[suggestion.name] = trial.suggest_int(
+                suggestion.name, suggestion.low, suggestion.high
+            )
 
-        if self.int_suggestions:
-            for suggestion in self.int_suggestions:
-                mean_params[suggestion.name] = int(
-                    np.mean(best_params[suggestion.name])
-                )
-        if self.float_suggestions:
-            for suggestion in self.float_suggestions:
-                mean_params[suggestion.name] = np.mean(best_params[suggestion.name])
-        if self.categorical_suggestions:
-            for suggestion in self.categorical_suggestions:
-                values, counts = np.unique(
-                    best_params[suggestion.name], return_counts=True
-                )
-                mean_params[suggestion.name] = values[np.argmax(counts)]
+        for suggestion in self.float_suggestions:
+            suggested_params[suggestion.name] = trial.suggest_float(
+                suggestion.name, suggestion.low, suggestion.high
+            )
+
+        for suggestion in self.categorical_suggestions:
+            suggested_params[suggestion.name] = trial.suggest_categorical(
+                suggestion.name, suggestion.choices
+            )
+
+        return suggested_params
+
+    def get_mean_params(self, best_params: dict, model_name: str) -> dict:
+        mean_params = {"model": model_name}
+
+        for suggestion in self.int_suggestions:
+            mean_params[suggestion.name] = int(np.mean(best_params[suggestion.name]))
+
+        for suggestion in self.float_suggestions:
+            mean_params[suggestion.name] = np.mean(best_params[suggestion.name])
+
+        for suggestion in self.categorical_suggestions:
+            values, counts = np.unique(best_params[suggestion.name], return_counts=True)
+            mean_params[suggestion.name] = values[np.argmax(counts)]
 
         return mean_params
+
+    def _process_model_before_fit(self, model):
+        return model
 
     def render(self, **kwargs) -> str:
         raise NotImplementedError
@@ -110,24 +99,20 @@ class ModelBaseWrapper:
 
 class DailyMeanModelWrapper(ModelBaseWrapper):
     def __init__(self, int_suggestions: list[IntSuggestions]):
-        self.estimator = DailyMeanModel
-
-        super().__init__(int_suggestions=int_suggestions, estimator=self.estimator)
+        super().__init__(estimator=DailyMeanModel, int_suggestions=int_suggestions)
 
     @staticmethod
     def render(**kwargs) -> str:
-        return f"DailyMeanModel({kwargs['window']})"
+        return f"DailyMeanModel(window={kwargs['window']})"
 
 
 class DayOfWeekMeanModelWrapper(ModelBaseWrapper):
     def __init__(self, int_suggestions: list[IntSuggestions]):
-        self.estimator = DayOfWeekMeanModel
-
-        super().__init__(int_suggestions=int_suggestions, estimator=self.estimator)
+        super().__init__(estimator=DayOfWeekMeanModel, int_suggestions=int_suggestions)
 
     @staticmethod
     def render(**kwargs) -> str:
-        return f"DOWMeanModel(weekdays: {kwargs['weekdays_window']}, weekends: {kwargs['weekends_window']})"
+        return f"DOWMeanModel(weekdays={kwargs['weekdays_window']}, weekends={kwargs['weekends_window']})"
 
 
 class ProphetWrapper(ModelBaseWrapper):
@@ -137,51 +122,38 @@ class ProphetWrapper(ModelBaseWrapper):
         float_suggestions: list[FloatSuggestions] = None,
         categorical_suggestions: list[CategoricalSuggestions] = None,
         extra_regressors: list[str] = None,
-        holidays=None,
-        horizon="16 days",
         initial="730 days",
+        model_base_params: dict = None,
     ):
-        self.extra_regressors = extra_regressors
-        self.holidays = holidays
-        self.horizon = horizon
+        self.extra_regressors = extra_regressors or []
         self.initial = initial
-
-        self.daily_seasonality = False
-        self.weekly_seasonality = True
-        self.yearly_seasonality = True
-        self.uncertainty_samples = False
-
-        self.model_params = {
-            "daily_seasonality": self.daily_seasonality,
-            "weekly_seasonality": self.weekly_seasonality,
-            "yearly_seasonality": self.yearly_seasonality,
-            "uncertainty_samples": self.uncertainty_samples,
-            "holidays": self.holidays,
-        }
 
         super().__init__(
             estimator=Prophet,
-            model_base_params=self.model_params,
+            model_base_params=model_base_params,
             int_suggestions=int_suggestions,
             float_suggestions=float_suggestions,
             categorical_suggestions=categorical_suggestions,
         )
 
     def _process_model_before_fit(self, model: Prophet) -> Prophet:
-        if self.extra_regressors is not None:
-            for regressor in self.extra_regressors:
-                model.add_regressor(regressor)
+        for regressor in self.extra_regressors:
+            model.add_regressor(regressor)
         return model
 
     def objective(
-        self, trial: optuna.Trial, train: pd.DataFrame, cutoffs: pd.DataFrame
+        self,
+        trial: optuna.Trial,
+        train: pd.DataFrame,
+        cutoffs: pd.DataFrame,
+        horizon: str,
     ) -> float:
         model = self.get_model(**self.suggest_params(trial))
-        model.fit(train[train["ds"] > train["ds"].max() - pd.Timedelta(self.initial)])
+        model.fit(train)
 
         df_cv = cross_validation(
             model,
-            horizon=self.horizon,
+            horizon=horizon,
             initial=self.initial,
             cutoffs=cutoffs,
             disable_tqdm=True,
