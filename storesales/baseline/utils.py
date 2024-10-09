47a5919a33225186badd70a2a2724be0725c5af6
tikhon <tikhon.radk@gmail.com>
2024-10-09 19:16:25 +0300

    make_time_series_split func created; need to test

--- storesales/baseline/utils.py
+++ storesales/baseline/utils.py
@@ -1,11 +1,14 @@
+from collections import defaultdict
 from itertools import product
 import random
 
+from tqdm import tqdm
 import numpy as np
 import pandas as pd
 
 import optuna
 
+from storesales.baseline.loss import rmsle
 from storesales.baseline.sales_predictor import SalesPredictor
 from storesales.constants import (
     EXTERNAL_TRAIN_PATH,
@@ -14,6 +17,86 @@ from storesales.constants import (
     EXTERNAL_OIL_PATH,
     EXTERNAL_HOLIDAYS_EVENTS_PATH,
 )
+
+
+def make_time_series_split(
+    df: pd.DataFrame,
+    cutoffs: list[pd.Timestamp],
+    test_size: int = 16,
+):
+    families = df["family"].unique()
+    stores = df["store_nbr"].unique()
+    dataset = {
+        "train": defaultdict(list),
+        "test": defaultdict(list),
+    }
+    test_period = pd.Timedelta(days=test_size)
+
+    for family, store in tqdm(product(families, stores)):
+        store_family_df = df[(df["family"] == family) & (df["store_nbr"] == store)]
+
+        for cutoff in cutoffs:
+            train_data = store_family_df[store_family_df["ds"] < cutoff]
+            test_data = store_family_df[
+                (store_family_df["ds"] >= cutoff) & (store_family_df["ds"] < cutoff + test_period)
+                ]
+
+            dataset["train"][(store, family)].append(train_data)
+            dataset["test"][(store, family)].append(test_data)
+
+    return dataset
+
+
+def run_study(df: pd.DataFrame, predictor: SalesPredictor, test_size: int = 16):
+    stores = df["store_nbr"].unique()
+    dataset = make_time_series_split(df, predictor.outer_cutoffs, test_size)
+
+    for family_group in predictor.family_groups:
+        store_family_groups = list(product(stores, family_group))
+        n_choices = predictor.get_n_store_family_choices(family_group)
+
+        for store, family in random.sample(store_family_groups, n_choices):
+            print(f"\n\nFamily: {family} - Store: {store}")
+
+            outer_results = []
+            for i_fold, outer_train in enumerate(dataset["train"][(store, family)]):
+
+                study = optuna.create_study(direction="minimize")
+                study.optimize(
+                    lambda trial: predictor.objective(trial, outer_train),
+                    **predictor.optuna_optimize_kwargs,
+                )
+
+                test_loss = []
+                for _store, _family in store_family_groups:
+                    outer_test = dataset["test"][(_store, _family)][i_fold]
+                    model = predictor.get_best_model(study.best_params)
+                    model.fit(outer_train)
+                    forecast = model.predict(outer_test)
+                    y_pred = forecast["yhat"].values
+                    y_true = outer_test["y"].values
+                    loss = rmsle(y_true, y_pred)
+                    test_loss.append(loss)
+                fold_test_loss = np.mean(test_loss)
+
+                predictor.evaluate_and_save_tune(
+                    family_group=family_group,
+                    best_params=study.best_params,
+                    loss=fold_test_loss
+                    # train=outer_train.copy(),
+                    # test=outer_test.copy(),
+                )
+                outer_results.append(fold_test_loss)
+                print(
+                    f"Outer: {predictor.render_model(study.best_params)} - RMSLE: {fold_test_loss}"
+                )
+
+            final_outer_rmsle = np.mean(outer_results)
+            print(f"\nFamily: {family} - Store: {store} RMSLE: {final_outer_rmsle}")
+
+        predictor.log_best(family_group)
+
+    return predictor
 
 
 def load_baseline_data():
@@ -55,46 +138,3 @@ def load_baseline_data():
 
 def load_submission():
     return pd.read_csv(EXTERNAL_SAMPLE_SUBMISSION_PATH, index_col="id")
-
-
-def run_study(df: pd.DataFrame, predictor: SalesPredictor, n_stores: int = 54):
-    stores = np.arange(1, n_stores + 1)
-
-    for family_group in predictor.family_groups:
-        store_family_groups = list(product(stores, family_group))
-        n_choices = predictor.get_n_store_family_choices(family_group)
-
-        for store, family in random.sample(store_family_groups, n_choices):
-            condition = (df["store_nbr"] == store) & (df["family"] == family)
-            train = df[condition][["ds", "y", "dcoilwtico"]].copy()
-
-            print(f"\n\nFamily: {family} - Store: {store}")
-
-            outer_results = []
-            for outer_train_index, outer_test_index in predictor.outer_cv.split(train):
-                outer_train = train.iloc[outer_train_index]
-                outer_test = train.iloc[outer_test_index]
-
-                study = optuna.create_study(direction="minimize")
-                study.optimize(
-                    lambda trial: predictor.objective(trial, outer_train),
-                    **predictor.optuna_optimize_kwargs,
-                )
-
-                rmsle_loss = predictor.evaluate_and_save_tune(
-                    family_group=family_group,
-                    best_params=study.best_params,
-                    train=outer_train.copy(),
-                    test=outer_test.copy(),
-                )
-                outer_results.append(rmsle_loss)
-                print(
-                    f"Outer: {predictor.render_model(study.best_params)} - RMSLE: {rmsle_loss}"
-                )
-
-            final_outer_rmsle = np.mean(outer_results)
-            print(f"\nFamily: {family} - Store: {store} RMSLE: {final_outer_rmsle}")
-
-        predictor.log_best(family_group)
-
-    return predictor
