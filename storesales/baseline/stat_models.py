477bcdb89b748c0347cdb59403e1904b7b5f0774
tikhon <tikhon.radk@gmail.com>
2024-10-09 10:47:00 +0300

    refactoring

--- storesales/baseline/stat_models.py
+++ storesales/baseline/stat_models.py
@@ -4,39 +4,41 @@ import pandas as pd
 class DailyMeanModel:
     def __init__(self, window: int):
         self.window = window
-        self.prediction = None
+        self.mean_prediction = None
 
     def fit(self, train: pd.DataFrame) -> None:
-        self.prediction = train["y"].tail(self.window).mean()
+        self.mean_prediction = train["y"].tail(self.window).mean()
 
     def predict(self, future: pd.DataFrame) -> pd.DataFrame:
-        if self.prediction is None:
-            raise ValueError("Model not fitted")
-        prediction = future.copy()
-        prediction["yhat"] = [self.prediction] * len(future)
-        return prediction
+        if self.mean_prediction is None:
+            raise ValueError("Model not fitted yet.")
+        future_predictions = future.copy()
+        future_predictions["yhat"] = [self.mean_prediction] * len(future)
+        return future_predictions
 
 
 class DayOfWeekMeanModel:
     def __init__(self, weekdays_window: int, weekends_window: int):
         self.weekdays_window = weekdays_window
         self.weekends_window = weekends_window
-        self.prediction = None
+        self.mean_predictions = None
 
     def fit(self, train: pd.DataFrame) -> None:
-        weekdays = train[train["ds"].dt.weekday < 5]
-        weekends = train[train["ds"].dt.weekday >= 5]
-        self.prediction = {
-            "weekdays": weekdays["y"].tail(self.weekdays_window).mean(),
-            "weekends": weekends["y"].tail(self.weekends_window).mean(),
+        weekdays_data = train[train["ds"].dt.weekday < 5]
+        weekends_data = train[train["ds"].dt.weekday >= 5]
+        self.mean_predictions = {
+            "weekdays": weekdays_data["y"].tail(self.weekdays_window).mean(),
+            "weekends": weekends_data["y"].tail(self.weekends_window).mean(),
         }
 
     def predict(self, future: pd.DataFrame) -> pd.DataFrame:
-        if self.prediction is None:
-            raise ValueError("Model not fitted")
-        prediction = future.copy()
-        prediction["yhat"] = [
-            self.prediction["weekdays"] if day < 5 else self.prediction["weekends"]
+        if self.mean_predictions is None:
+            raise ValueError("Model not fitted yet.")
+        future_predictions = future.copy()
+        future_predictions["yhat"] = [
+            self.mean_predictions["weekdays"]
+            if day < 5
+            else self.mean_predictions["weekends"]
             for day in future["ds"].dt.weekday
         ]
-        return prediction
+        return future_predictions
