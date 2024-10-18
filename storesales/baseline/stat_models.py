d183f358ec03179e3397d07dc48a055eab59d482
tikhon <tikhon.radk@gmail.com>
2024-10-18 17:02:47 +0300

    WeightedDayMeanModel model created; need to test

--- storesales/baseline/stat_models.py
+++ storesales/baseline/stat_models.py
@@ -42,3 +42,69 @@ class DayOfWeekMeanModel:
             for day in future["ds"].dt.weekday
         ]
         return future_predictions
+
+
+class WeightedDayMeanModel:
+    def __init__(
+        self,
+        weeks_window: int,
+        months_window: int,
+        years_window: int,
+        week_weight: float,
+        month_weight: float,
+        year_weight: float,
+    ):
+        self.weeks_window = weeks_window
+        self.months_window = months_window
+        self.years_window = years_window
+        self.week_weight = week_weight
+        self.month_weight = month_weight
+        self.year_weight = year_weight
+        self.train = None
+
+    def fit(self, train: pd.DataFrame) -> None:
+        self.train = train.copy()
+
+    def _get_day_averages(self, date: pd.Timestamp):
+        # Get past data for the same day of the week, month, and year ago
+        week_ago_dates = [
+            date - pd.DateOffset(weeks=i) for i in range(1, self.weeks_window + 1)
+        ]
+        month_ago_dates = [
+            date - pd.DateOffset(months=i) for i in range(1, self.months_window + 1)
+        ]
+        year_ago_dates = [
+            date - pd.DateOffset(years=i) for i in range(1, self.years_window + 1)
+        ]
+
+        week_ago_data = self.train[self.train["ds"].isin(week_ago_dates)]["y"].mean()
+        month_ago_data = self.train[self.train["ds"].isin(month_ago_dates)]["y"].mean()
+        year_ago_data = self.train[self.train["ds"].isin(year_ago_dates)]["y"].mean()
+
+        return week_ago_data, month_ago_data, year_ago_data
+
+    def predict(self, future: pd.DataFrame) -> pd.DataFrame:
+        if self.train is None:
+            raise ValueError("Model not fitted yet.")
+
+        future_predictions = future.copy()
+
+        yhat_values = []
+        for future_date in future_predictions["ds"]:
+            week_avg, month_avg, year_avg = self._get_day_averages(future_date)
+
+            yhat = (
+                (week_avg * self.week_weight)
+                + (month_avg * self.month_weight)
+                + (year_avg * self.year_weight)
+            )
+            yhat_values.append(yhat)
+
+            # update train to use prediction for future predictions
+            self.train = pd.concat(
+                [self.train, pd.DataFrame({"ds": [future_date], "y": [yhat]})],
+                ignore_index=True,
+            )
+
+        future_predictions["yhat"] = yhat_values
+        return future_predictions
