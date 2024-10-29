e1acc558b823ec0f4a4ba1e209b4eabffe29d713
tikhon <tikhon.radk@gmail.com>
2024-10-29 14:22:14 +0300

    models and loss paths added

--- storesales/constants.py
+++ storesales/constants.py
@@ -5,6 +5,8 @@ from config import ROOT_PATH
 
 DATA_PATH = os.path.join(ROOT_PATH, "data")
 EXTERNAL_DATA_PATH = os.path.join(DATA_PATH, "external")
+LOSSES_DATA_PATH = os.path.join(DATA_PATH, "losses")
+MODELS_PATH = os.path.join(ROOT_PATH, "models")
 
 DATASET_FILES = (
     "train.csv",
