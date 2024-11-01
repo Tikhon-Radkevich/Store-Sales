5aa211325f5e8d6ebb3f4dae799f9d04df20017b
tikhon <tikhon.radk@gmail.com>
2024-11-01 15:14:30 +0300

    save submission func added

--- storesales/light_gbm/utils.py
+++ storesales/light_gbm/utils.py
@@ -6,8 +6,9 @@ from storesales.constants import SUBMISSIONS_PATH, EXTERNAL_SAMPLE_SUBMISSION_PA
 
 
 def save_submission(df: pd.DataFrame, file_name: str):
-    submission_df = pd.read_csv(EXTERNAL_SAMPLE_SUBMISSION_PATH)
-    submission_df["sales"] = df["sales"]
+    submission_df = pd.read_csv(EXTERNAL_SAMPLE_SUBMISSION_PATH, index_col="id")
+    submission_df["sales"] = df["yhat"]
+    print(submission_df.isna().sum())
 
     file_path = os.path.join(SUBMISSIONS_PATH, file_name)
-    submission_df.to_csv(file_path, index=False)
+    submission_df.to_csv(file_path, index=True)
