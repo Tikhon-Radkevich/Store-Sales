4e2ba6f7153c59f85fe8397a5ede4f9d83ac0bdb
tikhon <tikhon.radk@gmail.com>
2024-11-04 10:59:01 +0300

    setting 'id' index moved to save_submission func; train_test_split func moved out ou ipynb

--- storesales/light_gbm/utils.py
+++ storesales/light_gbm/utils.py
@@ -1,14 +1,26 @@
 import os
 
 import pandas as pd
+from darts import TimeSeries
 
 from storesales.constants import SUBMISSIONS_PATH, EXTERNAL_SAMPLE_SUBMISSION_PATH
 
 
 def save_submission(df: pd.DataFrame, file_name: str):
+    df = df.set_index("id")
+
     submission_df = pd.read_csv(EXTERNAL_SAMPLE_SUBMISSION_PATH, index_col="id")
     submission_df["sales"] = df["yhat"]
     print(submission_df.isna().sum())
 
     file_path = os.path.join(SUBMISSIONS_PATH, file_name)
     submission_df.to_csv(file_path, index=True)
+
+
+def train_test_split(series: dict[str, list[TimeSeries]], split_date: pd.Timestamp):
+    train_series = {}
+
+    for family, series_list in series.items():
+        train_series[family] = [s.drop_after(split_date) for s in series_list]
+
+    return train_series
