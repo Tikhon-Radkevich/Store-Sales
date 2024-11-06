af39dc71e920e4a603c91dc923a3d2da6af8cc8f
tikhon <tikhon.radk@gmail.com>
2024-11-07 00:07:34 +0300

    will be removed next commit.

--- storesales/light_gbm/fit_evaluate/fit_model.py
+++ storesales/light_gbm/fit_evaluate/fit_model.py
@@ -1,29 +1,44 @@
-from darts.timeseries import TimeSeries
-from darts.models import LightGBMModel
+# todo - remove this file
+
+# from darts.models import LightGBMModel
+
+# from storesales.light_gbm.dataset import FamilyDataset
+# from storesales.light_gbm
 
 
-def fit_light_gb_models(
-    family_series_dict: dict[str, TimeSeries],
-    future_covariates: dict[str, list[TimeSeries]],
-    past_covariates: dict[str, list[TimeSeries]] | None,
-) -> dict[str, LightGBMModel]:
-    if past_covariates is None:
-        past_covariates = {}
+# def fit_light_gb_model(dataset: FamilyDataset, **model_kwargs):
+#     light_gb_model = LightGBMModel(
+#         lags=24,
+#         lags_future_covariates=[i for i in range(-9, 1, 3)],
+#         lags_past_covariates=[i for i in range(-25, -15, 3)],
+#         **model_kwargs,
+#     )
+#
+#     inputs = {
+#         "series": dataset.series,
+#         "future_covariates": dataset.future_covariates,
+#         "past_covariates": dataset.past_covariates,
+#     }
+#
+#     light_gb_model.fit(**inputs)
+#
+#     return light_gb_model
 
-    light_gb_models = {}
 
-    for family, series in family_series_dict.items():
-        inputs = {
-            "series": series,
-            "future_covariates": future_covariates.get(family, None),
-            "past_covariates": past_covariates.get(family, None),
-        }
-        light_gb_models[family] = LightGBMModel(
-            lags=24,
-            lags_future_covariates=[i for i in range(-30, 1, 3)],
-            lags_past_covariates=[i for i in range(-31, -15, 3)],
-        )
-
-        light_gb_models[family].fit(**inputs)
-
-    return light_gb_models
+# def fit_light_gb_models(
+#     dataset: dict[str, FamilyDataset],
+#     **model_kwargs,
+# ) -> dict[str, LightGBMModel]:
+#     light_gb_models = {}
+#
+#     for family, family_dataset in dataset.items():
+#         light_gb_models[family] = LightGBMModel(
+#             lags=24,
+#             lags_future_covariates=[i for i in range(-9, 1, 3)],
+#             lags_past_covariates=[i for i in range(-25, -15, 3)],
+#             **model_kwargs,
+#         )
+#         inputs = family_dataset.get_train_inputs()
+#         light_gb_models[family].fit(**inputs)
+#
+#     return light_gb_models
