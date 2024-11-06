acd24b90c892e6e0cbfa725e1c9e5e7ac1b85dd6
tikhon <tikhon.radk@gmail.com>
2024-11-07 00:06:50 +0300

    stride feature added; update: FamilyDataset usage; now parallel is optional.

--- storesales/light_gbm/fit_evaluate/evaluate_models.py
+++ storesales/light_gbm/fit_evaluate/evaluate_models.py
@@ -1,42 +1,31 @@
 from joblib import Parallel, delayed
 
 import pandas as pd
-from darts import TimeSeries
 from darts.models import RegressionModel
 
 from storesales.light_gbm.loss import clipped_rmsle
-from storesales.constants import END_TRAIN_DATE, TRAIN_TEST_SPLIT_DATE
-
-
-eval_end_date = pd.Timestamp(END_TRAIN_DATE) - pd.Timedelta(days=16)
-series_test_range = pd.date_range(TRAIN_TEST_SPLIT_DATE, eval_end_date, freq="D")
+from storesales.light_gbm.dataset import FamilyDataset
 
 
 def evaluate(
-        family_to_series_dict: dict[str, list[TimeSeries]],
-        family_to_series_id_dict: dict[str, list[dict[str, int]]],
-        models: dict[str, RegressionModel],
-        future_covariates,
-        past_covariates
+    dataset: dict[str, FamilyDataset],
+    models: dict[str, RegressionModel],
+    evaluate_range: pd.DatetimeIndex,
+    stride=1,
+    parallel=False,
 ) -> pd.DataFrame:
     def evaluate_family(family: str) -> pd.DataFrame:
-        series = family_to_series_dict[family]
-        lgb_family_stores = [
-            element["store_nbr"] for element in family_to_series_id_dict[family]
-        ]
+        series = dataset[family].series
+
         multi_index = pd.MultiIndex.from_product(
-            [[family], lgb_family_stores], names=["family", "store_nbr"]
+            [[family], dataset[family].stores], names=["family", "store_nbr"]
         )
 
         family_losses = []
-        for test_date in series_test_range:
-            inputs = {
-                "series": [s.drop_after(test_date) for s in series],
-                "future_covariates": future_covariates[family],
-                "past_covariates": past_covariates[family],
-            }
-
+        for test_date in evaluate_range[::stride]:
+            inputs = dataset[family].get_cut_inputs(test_date)
             preds = models[family].predict(n=16, show_warnings=False, **inputs)
+
             true_values = [s.slice_intersect(p) for p, s in zip(preds, series)]
 
             loss = [
@@ -52,8 +41,9 @@ def evaluate(
         family_losses_df = pd.concat(family_losses, axis=1)
         return family_losses_df
 
-    losses = Parallel(n_jobs=-1)(
-        delayed(evaluate_family)(family) for family in family_to_series_dict.keys()
-    )
+    if parallel:
+        losses = Parallel(n_jobs=-1)(delayed(evaluate_family)(f) for f in models.keys())
+    else:
+        losses = [evaluate_family(f) for f in models.keys()]
 
     return pd.concat(losses)
