75c6d3c6db39ee6b68bdc46b292cd8ddd4e4234b
tikhon <tikhon.radk@gmail.com>
2024-11-07 12:15:14 +0300

    refactoring; commented code removed.

--- storesales/light_gbm/utils.py
+++ storesales/light_gbm/utils.py
@@ -10,18 +10,10 @@ def save_submission(df: pd.DataFrame, file_name: str):
 
     submission_df = pd.read_csv(EXTERNAL_SAMPLE_SUBMISSION_PATH, index_col="id")
     submission_df["sales"] = df["yhat"]
-    print(submission_df.isna().sum())
 
     file_path = os.path.join(SUBMISSIONS_PATH, file_name)
     submission_df.to_csv(file_path, index=True)
 
+    print(f"Submission saved to {file_path}")
 
-# todo: remove this function
-# def cut_train(dataset: dict[str:FamilyDataset], split_date: pd.Timestamp):
-#     train_dataset = {}
-#
-#     for family, family_dataset in dataset.items():
-#         train_family_dataset = family_dataset.drop_after(split_date)
-#         train_dataset[family] = train_family_dataset
-#
-#     return train_dataset
+    return submission_df
