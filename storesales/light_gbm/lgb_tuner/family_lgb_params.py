1b156b02e7110e57d3ad768f7ad4be4c44c4c307
tikhon <tikhon.radk@gmail.com>
2024-11-07 16:30:32 +0300

    random_state and categorical covs added (date and store features).

--- storesales/light_gbm/lgb_tuner/family_lgb_params.py
+++ storesales/light_gbm/lgb_tuner/family_lgb_params.py
@@ -6,9 +6,18 @@ import optuna
 @dataclass
 class FamilyLightGBMModelParams:
     kwargs = dict(
+        random_state=42,
         lags=24,
         lags_future_covariates=[i for i in range(-9, 1, 3)],
         lags_past_covariates=[i for i in range(-25, -15, 3)],
+        categorical_static_covariates=["city", "state", "type", "cluster"],
+        categorical_future_covariates=[
+            "day",
+            "month",
+            "year",
+            "day_of_week",
+            "day_of_year",
+        ],
         verbosity=-1,
         n_jobs=1,
     )
