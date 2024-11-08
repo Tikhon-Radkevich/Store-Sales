cbb8390b8c395019389f34f90bd26113e03ef313
tikhon <tikhon.radk@gmail.com>
2024-11-08 17:32:12 +0300

    refactoring; get_minimal_fc_parameters func added.

--- storesales/light_gbm/feature_engineering/ts_fresh_features.py
+++ storesales/light_gbm/feature_engineering/ts_fresh_features.py
@@ -1,27 +1,30 @@
 import pandas as pd
 
 from tsfresh import extract_features
+from tsfresh.feature_extraction import MinimalFCParameters
 from tsfresh.utilities.dataframe_functions import roll_time_series
 
-from storesales.light_gbm.param_dataclasses import (
-    TrainRollParam,
-    ExtractFeaturesParam,
-)
+from storesales.light_gbm.param_dataclasses import ExtractFeaturesParam
 
 
-def make_roll(df: pd.DataFrame, timeshift: int = 30) -> pd.DataFrame:
+def make_roll(
+    df: pd.DataFrame, cols_to_roll: list[str], timeshift: int = 30, n_jobs: int = 6
+) -> pd.DataFrame:
     df["store_family"] = df[["store_nbr", "family"]].astype(str).agg("-".join, axis=1)
     df.drop(columns=["store_nbr", "family"], inplace=True)
 
-    train_roll_param = TrainRollParam(
+    df_columns = ["store_family", "date"] + cols_to_roll
+    df = df[df_columns]
+
+    rolls_df = roll_time_series(
         df_or_dict=df,
         column_id="store_family",
         column_sort="date",
         max_timeshift=timeshift,
         min_timeshift=timeshift,
+        rolling_direction=1,
+        n_jobs=n_jobs,
     )
-
-    rolls_df = roll_time_series(**train_roll_param.__dict__)
     rolls_df.drop(columns="store_family", inplace=True)
 
     rolls_df.index = pd.MultiIndex.from_tuples(rolls_df["id"])
@@ -31,15 +34,12 @@ def make_roll(df: pd.DataFrame, timeshift: int = 30) -> pd.DataFrame:
     return rolls_df
 
 
-def make_roll_features(df: pd.DataFrame):
-    extract_features_param = ExtractFeaturesParam(
-        timeseries_container=df,
-        column_id="id",
-        column_sort="date",
-    )
-
+def make_roll_features(
+    extract_features_param: ExtractFeaturesParam, prefix: str
+) -> pd.DataFrame:
     features_df = extract_features(**extract_features_param.__dict__)
     features_df.index.names = ["store_family", "date"]
+    features_df.columns = [f"{prefix}_{col}" for col in features_df.columns]
 
     features_df.reset_index(level=["store_family", "date"], inplace=True)
     features_df[["store_nbr", "family"]] = features_df["store_family"].str.split(
@@ -48,3 +48,10 @@ def make_roll_features(df: pd.DataFrame):
     features_df.drop(columns=["store_family"], inplace=True)
 
     return features_df
+
+
+def get_minimal_fc_parameters() -> MinimalFCParameters:
+    fc_parameters = MinimalFCParameters()
+    del fc_parameters["length"]
+    del fc_parameters["absolute_maximum"]
+    return fc_parameters
