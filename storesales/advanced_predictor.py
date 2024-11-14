c2bf3cd2c003d2f66055ed6ab5456f04157184b8
tikhon <tikhon.radk@gmail.com>
2024-11-14 17:48:00 +0300

    update

--- storesales/advanced_predictor.py
+++ storesales/advanced_predictor.py
@@ -10,6 +10,7 @@ class AdvancedPredictor:
 
     def __init__(
         self,
+        loss_split_date_str: str,
         baseline_model_names: list[str],
         baseline_model_file_paths: list[str],
         baseline_train_df: pd.DataFrame,
@@ -18,6 +19,7 @@ class AdvancedPredictor:
         lightgbm_model_prediction_df: pd.DataFrame,
         lightgbm_model_name: str = "LightGBM",
     ):
+        self.loss_split_date_str = loss_split_date_str
         self.baseline_model_names = baseline_model_names
         self.baseline_model_file_paths = baseline_model_file_paths
         self.baseline_train_df = baseline_train_df
@@ -36,18 +38,57 @@ class AdvancedPredictor:
         self._combined_loss = self._get_combined_loss()
         self._combined_prediction = self._get_combined_prediction()
 
-    def get_optimal_prediction(self, models: list[str] = None):
-        combined_loss = self._combined_loss.copy()
+        self._loss_to_choose_model_df, self._test_loss_df = self._split_loss()
+
+    def _split_loss(self):
+        split_index = self._combined_loss.columns.get_loc(self.loss_split_date_str)
+        _loss_to_choose_model_df = self._combined_loss.iloc[:, :split_index]
+        _test_loss_df = self._combined_loss.iloc[:, split_index:]
+        return _loss_to_choose_model_df, _test_loss_df
+
+    def get_min_loss(self, test_loss: bool = True):
+        loss_df = self._test_loss_df if test_loss else self._loss_to_choose_model_df
+        family_store_mean_loss = loss_df.mean(axis=1).rename("loss")
+        min_loss_mean = (
+            family_store_mean_loss.groupby(["family", "store_nbr"]).min().mean()
+        )
+        return min_loss_mean
+
+    def get_mean_test_loss(self):
+        return self._test_loss_df.mean(axis=1).rename("loss").reset_index()
+
+    def get_mean_model_choose_loss(self):
+        return self._loss_to_choose_model_df.mean(axis=1).rename("loss").reset_index()
+
+    def get_optimal_prediction(
+        self, models: list[str] = None, lightgbm_drop_families: list[str] = None
+    ):
+        combined_loss = self._loss_to_choose_model_df.copy()
         if models is not None:
             model_condition = combined_loss.index.get_level_values("model").isin(models)
             combined_loss = combined_loss[model_condition]
 
         family_store_mean_loss = combined_loss.mean(axis=1).rename("loss")
+
+        if lightgbm_drop_families is not None:
+            family_con = family_store_mean_loss.index.get_level_values("family").isin(
+                lightgbm_drop_families
+            )
+            model_con = (
+                family_store_mean_loss.index.get_level_values("model")
+                == self.lightgbm_model_name
+            )
+            family_store_mean_loss[family_con & model_con] = float("inf")
+
         min_loss_ids = family_store_mean_loss.groupby(["family", "store_nbr"]).idxmin()
         return self._combined_prediction.loc[min_loss_ids].copy()
 
-    def make_family_loss_plot(self, family: str):
-        mean_loss = self._combined_loss.mean(axis=1).rename("loss").reset_index()
+    def make_family_loss_plot(self, family: str, test_loss: bool = True):
+        if test_loss:
+            mean_loss = self.get_mean_test_loss()
+        else:
+            mean_loss = self.get_mean_model_choose_loss()
+
         family_store_mean_loss = mean_loss.reset_index()
         family_store_mean_loss = family_store_mean_loss[
             family_store_mean_loss["family"] == family
@@ -72,8 +113,12 @@ class AdvancedPredictor:
         plt.tight_layout()
         plt.show()
 
-    def make_overall_family_loss_plot(self):
-        mean_loss = self._combined_loss.mean(axis=1).rename("loss").reset_index()
+    def make_overall_family_loss_plot(self, test_loss: bool = True):
+        if test_loss:
+            mean_loss = self.get_mean_test_loss()
+        else:
+            mean_loss = self.get_mean_model_choose_loss()
+
         family_mean_loss = (
             mean_loss.groupby(["family", "model"])["loss"].mean().reset_index()
         )
@@ -119,10 +164,12 @@ class AdvancedPredictor:
         return predictions
 
     def _get_combined_prediction(self):
-        combined_prediction = {
-            self.lightgbm_model_name: self.lightgbm_model_prediction_df
-        }
+        lightgbm_pred_df = self.lightgbm_model_prediction_df.rename(
+            columns={"date": "ds"}
+        )
+        combined_prediction = {self.lightgbm_model_name: lightgbm_pred_df}
         combined_prediction.update(self._baseline_predictions)
+
         combined_prediction_df = pd.concat(combined_prediction)
 
         combined_prediction_df.index.names = ["model", "index"]
