daef4dd0b47a354656601cae03dd25e9a0b1dc0e
tikhon <tikhon.radk@gmail.com>
2024-11-14 17:49:28 +0300

    refactoring: 'dataset' -> 'family_datasets'

--- storesales/light_gbm/lgb_tuner/lgb_model_tuner.py
+++ storesales/light_gbm/lgb_tuner/lgb_model_tuner.py
@@ -1,22 +1,22 @@
 from joblib import Parallel, delayed
 
+import pandas as pd
 import optuna
 from darts.models import LightGBMModel
 
 from storesales.light_gbm.lgb_tuner.family_lgb_params import FamilyLightGBMModelParams
 from storesales.light_gbm.fit_evaluate.evaluate_models import evaluate
 from storesales.light_gbm.dataset import FamilyDataset
-from storesales.constants import VALIDATION_DATE_RANGE
 
 
 class LightGBMModelTuner:
     def __init__(
         self,
-        dataset: dict[str, FamilyDataset],
+        family_datasets: dict[str, FamilyDataset],
         families: list[str],
         param_suggestor: FamilyLightGBMModelParams,
     ):
-        self.dataset = dataset
+        self.family_datasets = family_datasets
         self.families = families
         self.param_suggestor = param_suggestor
 
@@ -26,10 +26,16 @@ class LightGBMModelTuner:
         )  # todo: one progress bar
 
     def run_parallel_tune(
-        self, n_jobs: int, eval_stride: int = 5, n_trials: int = 10
+        self,
+        evaluate_range: pd.DatetimeIndex,
+        n_jobs: int,
+        eval_stride: int = 5,
+        n_trials: int = 10,
     ) -> None:
         tuned_studies = Parallel(n_jobs=n_jobs)(
-            delayed(self._parallel_optuna_study)(f, eval_stride, n_trials)
+            delayed(self._parallel_optuna_study)(
+                f, evaluate_range, eval_stride, n_trials
+            )
             for f in self.families
         )
         for family, study in zip(self.families, tuned_studies):
@@ -47,18 +53,24 @@ class LightGBMModelTuner:
         best_params = self.param_suggestor.suggest(best_trial)
 
         best_model = LightGBMModel(**best_params)
-        best_model.fit(**self.dataset[family].get_train_inputs())
+        best_model.fit(**self.family_datasets[family].get_train_inputs())
         return family, best_model
 
-    def _objective(self, trial: optuna.Trial, family: str, stride: int) -> float:
+    def _objective(
+        self,
+        trial: optuna.Trial,
+        family: str,
+        evaluate_range: pd.DatetimeIndex,
+        stride: int,
+    ) -> float:
         light_gb_model_kwargs = self.param_suggestor.suggest(trial)
 
         model = LightGBMModel(**light_gb_model_kwargs)
-        model.fit(**self.dataset[family].get_train_inputs())
+        model.fit(**self.family_datasets[family].get_train_inputs())
 
         objective_loss_df = evaluate(
-            dataset=self.dataset,
-            evaluate_range=VALIDATION_DATE_RANGE,
+            dataset=self.family_datasets,
+            evaluate_range=evaluate_range,
             models={family: model},
             stride=stride,
         )
@@ -66,13 +78,13 @@ class LightGBMModelTuner:
         return objective_loss_df.values.mean()
 
     def _parallel_optuna_study(
-        self, family: str, stride: int, n_trials: int
+        self, family: str, evaluate_range: pd.DatetimeIndex, stride: int, n_trials: int
     ) -> optuna.Study:
         optuna.logging.set_verbosity(optuna.logging.WARNING)
 
         study = self.studies_dict[family]
         study.optimize(
-            lambda trial: self._objective(trial, family, stride),
+            lambda trial: self._objective(trial, family, evaluate_range, stride),
             n_trials=n_trials,
             show_progress_bar=self._show_progress_bar_dict[family],
         )
