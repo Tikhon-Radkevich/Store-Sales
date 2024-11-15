81d5172753c1a09641b7fe217b6a0d4b02e1deb7
tikhon <tikhon.radk@gmail.com>
2024-11-15 15:03:27 +0300

    lightGBM constants added.

--- storesales/light_gbm/constants.py
+++ storesales/light_gbm/constants.py
@@ -1,8 +1,12 @@
 import pandas as pd
 
 
-TRAINING_DATA_THRESHOLD_TIMESTAMP = pd.Timestamp("2017-04-01")
-ROLLS_THRESHOLD_TIMESTAMP = pd.Timestamp("2015-02-01")
-START_TARGET_SERIES_TIMESTAMP = ROLLS_THRESHOLD_TIMESTAMP - pd.Timedelta(days=10)
+TRAINING_DATA_THRESHOLD_TIMESTAMP = pd.Timestamp("2015-05-01")
+ROLLS_THRESHOLD_TIMESTAMP = pd.Timestamp("2013-05-01")
+START_TARGET_SERIES_TIMESTAMP = ROLLS_THRESHOLD_TIMESTAMP - pd.Timedelta(days=30)
 
 FEATURES_TO_ROLL = ["sales", "onpromotion", "dcoilwtico"]
+
+STATIC_COLS = ["city", "state", "type", "cluster"]
+CAT_STATIC_COVS = ["city", "state", "type", "cluster", "store_nbr"]
+CAT_FUTURE_COVS = []
