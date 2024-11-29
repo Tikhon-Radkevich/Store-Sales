4d575d09e3cb9ee645fdba57d2a49a64038f3ec4
tikhon <tikhon.radk@gmail.com>
2024-11-29 21:09:50 +0300

    script to copy some data and models to  dir for demo; todo transform to script to run from console.

---
+++ scripts/deployment/deployment_config.py
@@ -0,0 +1,22 @@
+deployment_date = "2016-03-11"
+
+families_to_deploy = [
+    "HOME AND KITCHEN I",
+    "LIQUOR,WINE,BEER",
+    "MEATS",
+    "SCHOOL AND OFFICE SUPPLIES",
+]
+
+lightgbm_families = [
+    "HOME AND KITCHEN I",
+    "LIQUOR,WINE,BEER",
+    "MEATS",
+]
+lightgbm_suffixes = ["_default_model_mean_and_std_features", "", ""]
+
+# baseline_model_files = [
+#     "daily_predictor.pkl",
+#     "day_of_week_predictor.pkl",
+#     "weighted_day_predictor.pkl",
+# ]
+# baseline_model_names = ["daily", "day_of_week", "weighted_day"]
